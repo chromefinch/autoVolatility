292eb353e4b521843af5415089728067479494be
chromefinch <38330372+chromefinch@users.noreply.github.com>
2023-04-20 15:27:23 -0400

    run from linux compatibility
    I really suck at this

--- autoVolatility.py
+++ autoVolatility.py
@@ -1,38 +1,41 @@
 #!/usr/bin/python
-import Queue
+import queue
 import threading
 import time
 import sys, os, getopt
 
 from subprocess import Popen, PIPE
 
-queue = Queue.Queue()
+queue = queue.Queue()
 start = time.time()
 
-dump_plugins = ["dumpcerts", "dumpregistry", "dumpfiles", "dumpregistry", "servicediff"]
+dump_plugins = []
+#dump_plugins = ["windows.dumpcerts", "windows.dumpregistry", "windows.dumpfiles", "windows.dumpregistry", "windows.servicediff"]
 
-plugins = ["amcache", "auditpol", "cachedump", "clipboard", "cmdline", "cmdscan", "connections", "connscan", "consoles", "deskscan", "devicetree", "dlllist",
-            "envars", "getservicesids", "handles", "hashdump", "hibinfo", "hivelist", "hivescan", "iehistory", "ldrmodules", "lsadump", "malfind", "mbrparser", "memmap", "mftparser", "modules", "notepad", 
-            "privs", "pslist", "psscan", "pstree", "psxview", "qemuinfo", "servicediff", "sessions", "sockets", "sockscan", "ssdt", "strings", "svcscan", "symlinkscan", "thrdscan", "verinfo", "windows", "wintree"]
+plugins = [ "windows.malfind", "windows.cmdline", "windows.pstree", "windows.netscan", "windows.netstat", "windows.callbacks"]
+#plugins = ["amcache", "auditpol", "cachedump", "clipboard", "windows.cmdline", "cmdscan", "connections", "connscan", "consoles", "deskscan", "devicetree", "dlllist",
+#            "envars", "getservicesids", "handles", "hashdump", "hibinfo", "hivelist", "hivescan", "iehistory", "ldrmodules", "lsadump", "malfind", "mbrparser", "memmap", "mftparser", "modules", "notepad", 
+#            "privs", "pslist", "windows.psscan", "pstree", "psxview", "qemuinfo", "servicediff", "sessions", "sockets", "sockscan", "ssdt", "strings", "svcscan", "symlinkscan", "thrdscan", "verinfo", "windows", "wintree"]
 
-plugins_all = [ "amcache", "apihooks", "atoms", "atomscan", "auditpol", "bigpools", "bioskbd", "cachedump", "callbacks", "clipboard", "cmdline", "cmdscan", "connections", "connscan", "consoles", "crashinfo",
-                "deskscan", "devicetree", "dlldump", "dlllist", "driverirp", "drivermodule", "driverscan", "editbox", "envars", "eventhooks", "evtlogs", "filescan", 
-                "gahti", "gditimers", "gdt", "getservicesids", "getsids", "handles", "hashdump", "hibinfo", "hivelist", "hivescan", "hpakextract", "hpakinfo", "idt", "iehistory", "imagecopy", "imageinfo",
-                "joblinks", "kdbgscan", "kpcrscan", "ldrmodules", "lsadump", "malfind", "mbrparser", "memdump", "memmap", "messagehooks", "mftparser", "moddump", "modscan", "modules", "multiscan", "mutantscan",
-                "notepad", "objtypescan", "patcher", "printkey", "privs", "procdump", "pslist", "psscan", "pstree", "psxview", "qemuinfo", "raw2dmp", "sessions", "shellbags", "shimcache",
-                "shutdowntime", "sockets", "sockscan", "ssdt", "strings", "svcscan", "symlinkscan", "thrdscan", "threads", "timeliner", "timers", "truecryptmaster", "truecryptpassphrase", "truecryptsummary",
-                "unloadedmodules", "userassist", "userhandles", "vaddump", "vadinfo", "vadtree", "vadwalk", "vboxinfo", "verinfo", "vmwareinfo", "windows", "wintree", "wndscan"]
+plugins_all = [ "windows.malfind", "windows.cmdline", "windows.pstree", "windows.netscan", "windows.netstat", "windows.callbacks"]
+#plugins_all = [ "amcache", "apihooks", "atoms", "atomscan", "auditpol", "bigpools", "bioskbd", "cachedump", "callbacks", "clipboard", "cmdline", "cmdscan", "connections", "connscan", "consoles", "crashinfo",
+#                "deskscan", "devicetree", "dlldump", "dlllist", "driverirp", "drivermodule", "driverscan", "editbox", "envars", "eventhooks", "evtlogs", "filescan", 
+#                "gahti", "gditimers", "gdt", "getservicesids", "getsids", "handles", "hashdump", "hibinfo", "hivelist", "hivescan", "hpakextract", "hpakinfo", "idt", "iehistory", "imagecopy", "imageinfo",
+#                "joblinks", "kdbgscan", "kpcrscan", "ldrmodules", "lsadump", "malfind", "mbrparser", "memdump", "memmap", "messagehooks", "mftparser", "moddump", "modscan", "modules", "multiscan", "mutantscan",
+#                "notepad", "objtypescan", "patcher", "printkey", "privs", "procdump", "pslist", "psscan", "pstree", "psxview", "qemuinfo", "raw2dmp", "sessions", "shellbags", "shimcache",
+#                "shutdowntime", "sockets", "sockscan", "ssdt", "strings", "svcscan", "symlinkscan", "thrdscan", "threads", "timeliner", "timers", "truecryptmaster", "truecryptpassphrase", "truecryptsummary",
+#                "unloadedmodules", "userassist", "userhandles", "vaddump", "vadinfo", "vadtree", "vadwalk", "vboxinfo", "verinfo", "vmwareinfo", "windows", "wintree", "wndscan"]
 
 dump_noDir = ["hashdump"]
 
 class ThreadVol(threading.Thread):
     """Threaded Volatility"""
-    def __init__(self, queue, out_dir, memfile, profile, vol_path):
+    def __init__(self, queue, out_dir, memfile, vol_path):
         threading.Thread.__init__(self)
         self.queue = queue
         self.out_dir = out_dir
         self.memfile = memfile
-        self.profile = profile
+        self.profile = ""
         self.vol_path = vol_path
 
     def run(self):
@@ -41,22 +44,22 @@ class ThreadVol(threading.Thread):
             plugin = self.queue.get()
 
             # Create plugin dir
-            plugin_dir = self.out_dir+"/"+plugin
+            plugin_dir = self.out_dir
             if not os.path.exists(plugin_dir):
                 os.makedirs(plugin_dir)
             
             # Run volatility
             if ("dump" in plugin and not plugin in dump_noDir) or (plugin in dump_plugins):
-                cmd = self.vol_path+" -f "+ self.memfile+" --profile="+self.profile+" "+plugin+"  --dump-dir="+plugin_dir
+                cmd = self.vol_path+" -f "+ self.memfile+" "+plugin+"  --dump-dir="+plugin_dir
             else:
-                cmd = self.vol_path+" -f "+ self.memfile+" --profile="+self.profile+" "+plugin
-            print cmd
+                cmd = self.vol_path+" -f "+ self.memfile+" "+plugin
+            print(cmd)
             pw = Popen(cmd.split(), stdout=PIPE, stderr=PIPE)
             stdout,stderr = pw.communicate()
-            if stderr: print plugin + ": " + stderr
+            if stderr: print(stderr)
 
             # Write the output
-            with open(plugin_dir+"/"+plugin+".txt",'w') as f:
+            with open(plugin_dir+"/"+plugin+".txt",'wb') as f:
                 f.write(stdout)
 
             #signals to queue job is done
@@ -64,20 +67,7 @@ class ThreadVol(threading.Thread):
 
 
 
-# Get profile of a memfile
-def getProfile(file, vol_path):
-    cmd = vol_path+" -f "+file+" imageinfo"
-    print cmd
-    pw = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
-    stdout,stderr = pw.communicate()
-    print stderr
-    for line in stdout.split('\n'):
-        if "Suggested Profile(s)" in line:
-            profile = line.split(": ")
-            if len(profile) > 1:
-                profile = profile[1].split(",")[0].split("(")[0]
-                return profile
-    return ""
+
 
 
 def main(argv):
@@ -85,22 +75,22 @@ def main(argv):
     hlp = "autoVol.py -f MEMFILE -d DIRECTORY [-e VOLATILITY-PATH] [-a] [-p PROFILE] [-c 'plugin1,plugin2,plugin3']"
     try:
         opts, args = getopt.getopt(argv,"hf:d:p:c:ae:t:",["help","file","directory=","profile=","console=","all", "volatility-path=", "threads="])
-    except getopt.GetoptError, err:
-        print "~ %s" % str(err)
-        print hlp
+    except getopt.GetoptError as err:
+        print("~ %s" % str(err))
+        print(hlp)
         sys.exit(2)
 
-    memfile, console, profile, directory, use_all, vol_path, threads = "", "", "", "", False, "volatility", 8
+    memfile, console, directory, profile, use_all, vol_path, threads = "", "", "", "", False, "volatility", 8
 
     for opt, arg in opts:
         if opt == '-h':
-            print hlp
+            print(hlp)
             sys.exit()
 
         elif opt in ("-f","--file"):
             memfile = arg
             if not os.path.exists(memfile):
-                print "File in path "+memfile+" does not exists"
+                print("File in path "+memfile+" does not exists")
                 sys.exit()
         
         elif opt in ("-d","--directory"):
@@ -109,14 +99,13 @@ def main(argv):
                 try: 
                     os.makedirs(directory)
                 except:
-                    print "Not a directory or not enough permissions: "+directory
+                    print("Not a directory or not enough permissions: "+directory)
                     sys.exit()
             if not os.path.isdir(directory) or not os.access(directory, os.W_OK):
-                print "Not a directory or not enough permissions: "+directory
+                print("Not a directory or not enough permissions: "+directory)
                 sys.exit()
 
-        elif opt in ("-p","--profile"):
-            profile = arg
+
 
         elif opt in ("-c", "--console"):
             console = arg
@@ -133,17 +122,10 @@ def main(argv):
 
 
     if not directory:
-        print "Set a directory using the option -d"
-        print hlp    
+        print("Set a directory using the option -d")
+        print(hlp)    
         sys.exit()
 
-    # Get profile of the memfile
-    if profile == "":
-        profile = getProfile(memfile, vol_path)
-        if profile == "":
-            print "Not profile found! you can set the profile using the -p option"
-            sys.exit()
-    print "Using profile: "+profile
 
     #populate queue with data
     if console == "": # If not console, default plugins
@@ -164,8 +146,8 @@ def main(argv):
 
     #run X threads
     for i in range(threads):
-        t = ThreadVol(queue, directory, memfile, profile, vol_path)
-        t.setDaemon(True)
+        t = ThreadVol(queue, directory, memfile, vol_path)
+        t.daemon = True
         t.start()
         time.sleep(0.1)
 
@@ -173,7 +155,7 @@ def main(argv):
 
     #wait on the queue until everything has been processed     
     queue.join()
-    print "Elapsed Time: %s" % (time.time() - start)
+    print("Elapsed Time: %s" % (time.time() - start))
 
 
     
